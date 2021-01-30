4f1401e3c264f718be1d10123ddb7fed73ec4545
jromaris <jromaris@itba.edu.ar>
2021-01-30 15:12:33 -0300

    Modified NN and its input

--- objs/q_learning.py
+++ objs/q_learning.py
@@ -16,13 +16,13 @@ class DQN(tf.keras.Model):
     def __init__(self, num_actions):
         super(DQN, self).__init__()
 
-        self.input_lay = tf.keras.layers.InputLayer(input_shape=(32, 21+2, 41, 1))
+        self.input_lay = tf.keras.layers.InputLayer(input_shape=(32, 21, 41, 4))
         self.conv1 = tf.keras.layers.Conv2D(filters=32, kernel_size=(6, 6), strides=(1, 1), activation='tanh')
         self.conv2 = tf.keras.layers.Conv2D(filters=64, kernel_size=(2, 2), strides=(1, 1), activation='tanh')
         self.conv3 = tf.keras.layers.Conv2D(filters=96, kernel_size=(2, 2), strides=(1, 1), activation='tanh')
         self.flat = tf.keras.layers.Flatten()
-        self.dense1 = tf.keras.layers.Dense(512, activation='tanh')
-        self.dense2 = tf.keras.layers.Dense(512, activation='tanh')
+        self.dense1 = tf.keras.layers.Dense(1024, activation='tanh')
+        self.dense2 = tf.keras.layers.Dense(1024, activation='tanh')
         self.dense3 = tf.keras.layers.Dense(num_actions, activation='sigmoid')
 
     def call(self, x, training=True, mask=None):
