f3527732cb026ff6f8a2a43a6d0aa07dcc36e01c
taomasgonzalez <togonzalez@itba.edu.ar>
2021-02-15 00:56:04 -0300

    will start training

--- bubbles2.py
+++ bubbles2.py
@@ -285,11 +285,11 @@ def main(epsilon_pars, reward_pars, num_episodes=1000, batch_size=32, discount=0
     else:
         test(reward_pars)
 
-if __name__ == '__main__':
-
-    reward_params = {'game over': -200, 'no hit': -2, 'hit': 1, 'balls_down_positive': True, 'game won': 100}
-    epsilon_params = {'constant': (False, 0.7), 'a': 0, 'k': 0.75, 'b': 1.5, 'q': 0.5, 'v': 0.55, 'm': 0, 'c': 1}
-    # saved_model = tf.keras.models.load_model('models/model30', compile=False)
-    saved_model = None
-    main(epsilon_params, reward_params, num_episodes=1000, batch_size=32, discount=0.92, amount_frames=2000,
-        activation='relu', mod_n=0)
+# if __name__ == '__main__':
+# 
+#     reward_params = {'game over': -200, 'no hit': -2, 'hit': 1, 'balls_down_positive': True, 'game won': 100}
+#     epsilon_params = {'constant': (False, 0.7), 'a': 0, 'k': 0.75, 'b': 1.5, 'q': 0.5, 'v': 0.55, 'm': 0, 'c': 1}
+#     # saved_model = tf.keras.models.load_model('models/model30', compile=False)
+#     saved_model = None
+#     main(epsilon_params, reward_params, num_episodes=1000, batch_size=32, discount=0.92, amount_frames=2000,
+#         activation='relu', mod_n=0)
