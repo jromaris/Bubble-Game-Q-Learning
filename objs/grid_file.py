6cb0ec5975e93659a385ff8a6703b0f0721bf57d
jromaris <jromaris@itba.edu.ar>
2021-02-17 09:50:07 -0300

    REMOVED APPENDTOP

--- objs/grid_file.py
+++ objs/grid_file.py
@@ -83,7 +83,7 @@ class GridManager:
                 else:
                     self.curr_hit = False
 
-            self.appended_top = (self.collision_counter % APPEND_COUNTDOWN == 0) and (self.collision_counter != 0)
+            #self.appended_top = (self.collision_counter % APPEND_COUNTDOWN == 0) and (self.collision_counter != 0)
             self.updateRows()
             self.popCluster(bubble, game)
             self.findTargets()
@@ -192,8 +192,8 @@ class GridManager:
     def updateRows(self):
 
         # after 'APPEND_COUNTDOWN' of collisions, add a row to the top
-        if (self.collision_counter % APPEND_COUNTDOWN == 0) and (self.collision_counter != 0):
-            self.appendTop()
+        #if (self.collision_counter % APPEND_COUNTDOWN == 0) and (self.collision_counter != 0):
+        #    self.appendTop()
 
         # if theres an existent bubble in the very last row, add a new row to the bottom
         # A bullet takes the place of a non-existent bubble so there should always be an empty
